1cc58818691d6a4d67fcf385fb45c1d199d8868c
Munir Zaman <munir.zaman2k22@outlook.com>
2022-12-18 13:26:40 +0600

    install.sh

--- install.sh
+++ install.sh
@@ -2,38 +2,66 @@
 
 echo "Welcome to setup!"
 
+# Install nala
 echo "Installing nala"
-sudo apt install nala
+sudo apt install nala -y
 
+# Update
 echo "Update? [y|n]"
 read -r yesno
 if [[ $yesno == "y" ]]; then
-	sudo nala update
-	sudo nala upgrade
+	sudo nala update -y
+	sudo nala upgrade -y
 fi
 
 # Setup git and github
 echo "Installing git"
-sudo nala install git
+sudo nala install git -y
+git config --global user.email "munir.zaman2k22@outlook.com"
+git config --global user.name "Munir Zaman"
+
 echo "Connect to github! [y|n]"
 read -r yesno 
 if [[ $yesno == "y" ]]; then
-	sudo nala install gh
+	sudo nala install gh -y
 	gh auth login
 fi
 
 # install vim and neovim
 echo "Installing neovim and vim"
-sudo nala install neovim
-sudo nala install vim
+sudo nala install neovim -y
+sudo nala install vim -y
 
 # install python and pip
 echo "Install python3 and pip? [y|n]"
 read -r yesno
 if [[ $yesno == "y" ]]; then	
 	echo "Installing python3 and pip"
-	sudo nala install python3
-	sudo nala install python3-pip
+	sudo nala install python3 -y
+	sudo nala install python3-pip -y
+fi
+
+# install kitty, zsh, oh-my-zsh and pure theme
+echo "Install kitty, zsh (set to default), oh-my-zsh and pure theme for zsh? [y|n]"
+read -r yesno
+if [[ $yesno == "y" ]]; then
+	echo "Installing kitty!"
+	sudo nala install kitty -y
+	echo "Installing zsh!" 
+	sudo nala install zsh -y
+	#change default shell
+	chsh -s $(which zsh)
+	echo "Installing ohmyzsh!"
+	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+	echo "Installing pure theme for zsh!"
+	mkdir -p "$HOME/.zsh"
+	git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+	# add pure theme to .zshrc
+	echo "Writting to .zshrc!"
+	echo "fpath+=($HOME/.zsh/pure)" >> $HOME/.zshrc
+	echo "autoload -U promptinit; promptinit" >> $HOME/.zshrc
+	echo "prompt pure" >> $HOME/.zshrc
 fi
 
 # install node for coc.nvim
@@ -47,31 +75,9 @@ if [[ $yesno == "y" ]]; then
 	nvm use node
 fi
 
-# install kitty, zsh, oh-my-zsh and pure theme
-echo "Installing kitty!"
-sudo nala install kitty
-
-echo "Installing zsh!"
-sudo nala install zsh
-chsh -s $(which zsh)
-#change default shell
-
-echo "Installing ohmyzsh!"
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "Installing pure theme for zsh!"
-mkdir -p "$HOME/.zsh"
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
-
-echo "Writting to .zshrc!"
-echo "fpath+=($HOME/.zsh/pure)" >> $HOME/.zshrc
-echo "autoload -U promptinit; promptinit" >> $HOME/.zshrc
-echo "prompt pure" >> $HOME/.zshrc
-
 # install JetBrainsMono Nerd Font
 echo "Install JetBrainsMono Nerd Font? [y|n]"
 read -r yesno 
-
 if [[ $yesno == "y" ]]; then
 	mkdir ~/.fonts
 	wget -P ~/.fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
