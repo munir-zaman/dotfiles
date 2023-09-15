572d006da99e992b3efc62e78653e7ce6b819ff0
Munir Zaman <munir.zaman2k22@outlook.com>
2023-09-15 18:07:46 +0600

    blur and border -> picom

--- scripts/i3-dev.sh
+++ scripts/i3-dev.sh
@@ -4,3 +4,6 @@ sudo apt install apt-transport-https --yes
 echo "deb https://baltocdn.com/i3-window-manager/i3/i3-autobuild-ubuntu/ all main" | sudo tee /etc/apt/sources.list.d/i3-autobuild.list
 sudo apt update
 sudo apt install i3
+
+# screenshot tools
+sudo nala install maim sclip copyq
