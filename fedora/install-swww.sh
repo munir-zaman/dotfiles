344e0b1f58ff8fa28208c94277db937634e28c95
Munir Zaman <munir.zaman2k22@outlook.com>
2024-06-16 13:14:46 +0600

    removed wal and swww

--- fedora/install-swww.sh
+++
@@ -1,7 +0,0 @@
-!/bin/sh
-
-sudo dnf5 install lz4 lz4-devel
-
-git clone https://github.com/LGFae/swww
-cd swww
-cargo build --release
--- fedora/pywal.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-sudo dnf5 install python3-pip
-# https://github.com/dylanaraps/pywal
-sudo pip3 install pywal
-# REF: https://github.com/Frewacom/pywalfox
-sudo pip3 install pywalfox
-pywalfox install
