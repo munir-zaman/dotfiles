cacf7142e8d41fe5d90bc266f248592952eb9a54
Munir Zaman <munir.zaman2k22@outlook.com>
2025-06-09 22:02:53 +0600

    fedora 42

--- fedora/rpm.sh
+++ fedora/rpm.sh
@@ -2,9 +2,19 @@
 # See (https://rpmfusion.org/Configuration) for more info
 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf config-manager --enable fedora-cisco-openh264
-sudo dnf groupupdate core
+
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+
+sudo dnf update @core
+sudo dnf install rpmfusion-\*-appstream-data
+
+# See (https://rpmfusion.org/Howto/Multimedia) for more info
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf groupupdate sound-and-video
-sudo dnf install nvidia-vaapi-driver
+sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf install libva-nvidia-driver
+
+sudo dnf install rpmfusion-nonfree-release-tainted
+sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware"
+
+# sudo dnf groupupdate sound-and-video
+# sudo dnf install nvidia-vaapi-driver
